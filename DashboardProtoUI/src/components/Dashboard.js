6d489007d00ed669ce01051af7ff81a8c43c8068
Lunettes0910 <tnminh98@gmail.com>
2022-03-03 01:56:18 -0500

    Top navigation + editing sidebar navigation

--- DashboardProtoUI/src/components/Dashboard.js
+++ DashboardProtoUI/src/components/Dashboard.js
@@ -1,17 +1,21 @@
-import React, { useEffect, useState } from "react"; 
+import React, { useEffect, useState } from "react";
+import {Button, FormControl, InputGroup} from "react-bootstrap";
 import { getAllTest } from "../services";
-import company_icon from '../images/company_icon.png';
-import user_icon from '../images/user_icon.png';
-import dashboard from '../images/dashboard.png';
-import risks from '../images/risks.png';
-import vulnerabilities from '../images/vulnerabilities.png';
-import threats from '../images/threats.png';
-import assets from '../images/assets.png';
-import recommendations from '../images/recommendations.png';
-import reports from '../images/reports.png';
-import settings from '../images/settings.png';
+import company_icon from '../images/user/company_icon.png';
+import user_icon from '../images/user/user_icon.png';
+import dashboard_a from '../images/icons/dashboard_icon_a.png';
+import risks from '../images/icons/risk_icon.png';
+import vulnerabilities from '../images/icons/vulner_icon.png';
+import threats from '../images/icons/threat_icon.png';
+import assets from '../images/icons/asset_icon.png';
+import recommendations from '../images/icons/rec_icon.png';
+import reports from '../images/icons/report_icon.png';
+import settings from '../images/icons/setting_icon.png';
+import search from '../images/icons/search_icon.png';
+import notification from '../images/icons/noti_icon.png';
+import info from '../images/icons/info_icon.png';
+import vendor_icon from '../images/icons/vendor_icon.png';
 
- 
 
 export const Dashboard = () => {
  
@@ -37,95 +41,78 @@ export const Dashboard = () => {
         });
       }, []);
     return (
- <div class="container-fluid">
-    <div class="Rectangle-48">   
-     <div class="Side-Bar-Main--Profile-2432">
-       <div id="sidenav-1" class="sidenav" data-mdb-color="dark" role="navigation" data-mdb-hidden="false"
-           data-mdb-accordion="true">
-            <a class="ripple d-flex justify-content-center py-3" href="#" data-mdb-ripple-color="primary">
-            <img id="company_icon" src={company_icon}
-              alt="Company Logo" draggable="false"/>
-            </a>
-            <p class="Company-Name">
-              Company Name
-            </p>
-              <a class="ripple d-flex justify-content-center py-3" href="#" data-mdb-ripple-color="primary">
-              <img id="user_icon" src={user_icon}
-                alt="User" draggable="false"/>
-              </a>
-              <p class="User-Name">
-                User Name
-              </p>
+      <div class="db-site-container"> 
+        <div class="db-container db-sidenav">
+          <div class="db-sidenav-profile" data-mdb-color="dark" role="navigation" data-mdb-hidden="false"
+              data-mdb-accordion="true">
+            <div class="user-info">
+              <img id="company-icon" src={company_icon} alt="Company Logo" draggable="false"/>
+              <p class="user-label">Company Name</p>
+            </div>
+            <div class="user-info">
+              <img id="user-icon" src={user_icon} alt="User" draggable="false"/>
+              <p class="user-label">Alex Toma</p>
+            </div>
             <ul class="sidenav-menu">
-              <li class="sidenav-item">
-                <a class="sidenav-link active" href="#">
-                  <img src={dashboard} alt =""/>
-                  <span class="icon-name-orange">
-                    Dashboard
-                  </span>
-                </a>                
-              </li>
-              <li class="sidenav-item">
-                  <a class="sidenav-link" href="#">
-                    <img src={risks} alt =""/>
-                    <span class="icon-name">
-                      Risks
-                    </span>
-                  </a>                  
+              <li class="sidenav-item sidenav-active">
+                <a class="sidenav-link" href="#">
+                  <img class="sidenav-icon" src={dashboard_a} alt =""/>Dashboard
+                </a>   
               </li>
               <li class="sidenav-item">
                 <a class="sidenav-link" href="#">
-                  <img src={vulnerabilities} alt =""/>
-                  <span class="icon-name">
-                    Vulnerabilities
-                  </span>
+                  <img class="sidenav-icon" src={risks} alt =""/>Risks
                 </a>
               </li>
               <li class="sidenav-item">
                 <a class="sidenav-link" href="#">
-                  <img src={threats} alt =""/>
-                  <span class="icon-name">
-                    Threats
-                  </span>
+                  <img class="sidenav-icon" src={vulnerabilities} alt =""/>Vulnerabilities
+                </a>
+              </li>
+              <li class="sidenav-item">
+                <a class="sidenav-link" href="#">
+                  <img class="sidenav-icon" src={threats} alt =""/>Threats
                 </a>
              </li>
              <li class="sidenav-item">
                 <a class="sidenav-link" href="#">
-                  <img src={assets} alt =""/>
-                  <span class="icon-name">
-                     Assets
-                  </span>
+                  <img class="sidenav-icon" src={assets} alt =""/>Assets
                 </a>
               </li>
               <li class="sidenav-item">
                 <a class="sidenav-link" href="#">
-                  <img src={recommendations} alt =""/>
-                  <span class="icon-name">
-                    Recommendations
-                  </span>
+                  <img class="sidenav-icon" src={recommendations} alt =""/>Recommendations
                 </a>
               </li>
               <li class="sidenav-item">
                 <a class="sidenav-link" href="#">
-                  <img src={reports} alt =""/>
-                  <span class="icon-name">
-                    Reports
-                  </span>
+                  <img class="sidenav-icon" src={reports} alt =""/>Reports
                 </a>
               </li>
               <li class="sidenav-item">
                 <a class="sidenav-link" href="#">
-                  <img src={settings} alt =""/>
-                  <span class="icon-name">
-                    Settings
-                  </span>
+                  <img class="sidenav-icon" src={settings} alt =""/>Settings
                 </a>
               </li>
             </ul>
           </div>
         </div>
+        <div class="db-container-main">
+          <div class="db-container db-topnav">
+            <InputGroup className="db-search">
+              <input type="text" placeholder="Search" aria-label="Search" />
+              <Button variant="outline-secondary" id="button-addon2">
+                <img src={search}/>
+              </Button>
+            </InputGroup>
+            <div class="menu-nav">
+              <img id="vendor" src={vendor_icon} alt="Valencia" draggable="false"/>
+              <img class="topnav-icon" src={info} alt="Help" draggable="false"/>
+              <img class="topnav-icon" src={notification} alt="Notifications" draggable="false"/>
+            </div>
+          </div>
+        </div>
       </div>
-    </div> 
   );
 }
 export default Dashboard;
