608a36177f6b3c9e5298d72906eafa8874ff1440
Tina <tinasylviac@gmail.com>
2022-03-12 23:32:50 -0500

    Redux architecture and login state

--- DashboardProtoUI/src/components/Login.js
+++ DashboardProtoUI/src/components/Login.js
@@ -1,46 +1,33 @@
 import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from 'react-redux';
 import {useHistory} from 'react-router-dom'
 import {Button, Form} from "react-bootstrap";
 import { getAllTest } from "../services";
 import vendor_icon from '../images/icons/vendor_icon.png';
 import { DASHBOARD } from "../navigation/CONSTANTS";
+import { login } from '../actions/userActions';
 
 export const Login = (props) => {
- 
-  const [tests, setTests] = useState(null);
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo, loading, error } = userLogin;
+
+  const dispatch = useDispatch();
+  const submitHandler = (e) => {
+    e.preventDefault();
+    dispatch(login(email, password));
+    props.history.push('/dashboard');
+  };
 
   const history =useHistory();
   const onLogin =()=>{
   history.push({
      pathname: DASHBOARD,
-     state: {
-      
-     },
-   });
-  }
-    
 
-    useEffect(() => {
-        return new Promise((resolve, reject) => {
-          try {
-            // do db call  or API endpoint axios call here and return the promise.
-           getAllTest()
-            .then((res) => {
-              setTests(res);
-              //resolve(res);
-            })
-              .catch((err) => {
-                setTests([]); 
-                reject("Request error!");
-              });
-          } catch (error) {
-            console.error("GetTest error!==", error);
-            reject("Test error!");
-          }
-        });
-      }, []);
+   });
+  }  
     return (
         <div class="login-container">
             <div class="login-welcome">
@@ -50,16 +37,16 @@ export const Login = (props) => {
             </div>
             <div class="login-form-container">
               <p>Don&apos;t have an account? <a href="#">Get in touch to get started.</a></p>
-              <Form>
+              <Form onSubmit={submitHandler}>
                 <Form.Group className="mb-4" controlId="formBasicEmail">
                   <Form.Label>Email</Form.Label>
-                  <Form.Control type="email" id="email" name='email' placeholder="Enter email"  required onChange={(e) => setEmail(e.target.value)} />
+                  <Form.Control type="email" id="email" value={email} placeholder="Enter email"  required onChange={(e) => setEmail(e.target.value)} />
                 </Form.Group>
                 <Form.Group className="mb-4" controlId="formBasicPassword">
                   <Form.Label>Password</Form.Label>
-                  <Form.Control type="password"  id="password" name='password' placeholder="Enter password"  required  onChange={(e) => setPassword(e.target.value)} />
+                  <Form.Control type="password"  id="password" value={password} placeholder="Enter password"  required  onChange={(e) => setPassword(e.target.value)} />
                 </Form.Group>
-                <Button className="w-100" variant="primary" type="submit" onClick={() =>onLogin()}>Login</Button>
+                <Button className="w-100" variant="primary" type="submit"onClick={() =>onLogin()}>Login</Button>
               </Form>
             </div>
         </div>
