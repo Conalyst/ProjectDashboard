b87dac1394dba6fa085e3d3e5afb9fa20d7acfdd
Tina <tinasylviac@gmail.com>
2022-03-26 16:21:34 -0400

    Frontend to tmp branch

--- DashboardProtoUI/src/navigation/RouterConfig.js
+++ DashboardProtoUI/src/navigation/RouterConfig.js
@@ -1,33 +1,6 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 import { NotFound } from "./NotFound";
-<<<<<<< HEAD
-import Test from '../components/Test'
-import Login from '../components/login'
-import {
- LOGIN,
- TEST
-} from "./constants";
-export const RouterConfig = (props) => {
- 
-  return (
-    <Switch>
-   
-      <Route exact path={TEST} >
-        <Test/>
-        </Route >
-      <Route exact path={LOGIN} >
-          <Login/>
-       </Route >
-      <Route path="*">
-        <NotFound />
-      </Route>
-    </Switch>
-  );
-};
-
-export default RouterConfig;
-=======
 import Dashboard from "../components/Dashboard";
 import Login from '../components/Login'
 import Test from '../components/Test'
@@ -56,4 +29,3 @@ export const RouterConfig = (props) => {
         </Switch>
     );
 };
->>>>>>> Frontend
