1d29244c926104aa754b38185fca4ceb5479df2b
Rupi-Jain <rupi.jain@gmail.com>
2022-03-26 17:38:33 -0400

    Role and User models

--- DashboardProtoAPI/index.ts
+++ DashboardProtoAPI/index.ts
@@ -20,7 +20,7 @@ const origin = {
 
 
 const port = process.env.PORT || 3002;
- db.sequelize.sync({ alter: true }).then(() => {
+ db.sequelize.sync({ force: true }).then(() => {
   console.log("Welcome.......")
   app.listen(port, () => console.log(`App listening on PORT ${port}`));
  })
