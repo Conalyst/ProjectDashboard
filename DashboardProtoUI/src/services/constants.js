eb832ee9cae5d551fd7c76eb0db2bc1a84fdd677
Tina <tinasylviac@gmail.com>
2022-03-27 19:22:49 -0400

    updating routes

--- DashboardProtoUI/src/services/constants.js
+++ DashboardProtoUI/src/services/constants.js
@@ -1,3 +1,4 @@
 export const BASE_ADDRESS = () => 'http://localhost:3002/api'
 export const GET_ALL_TEST = () => `${BASE_ADDRESS()}/v2/tests`
 export const LOGIN = ()=>BASE_ADDRESS()+'/v2/login';
+export const ASSET = ()=>BASE_ADDRESS()+'/v2/dashboard/asset';
