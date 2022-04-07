c6af3b8d4cc4418561ed50fa10774f75158eb533
Hadjira <bkh.hadjira@gmail.com>
2022-04-07 05:12:55 -0400

    add CONSTANTS

--- DashboardProtoUI/src/navigation/constants.js
+++ DashboardProtoUI/src/navigation/constants.js
@@ -1,3 +1,7 @@
 export const LOGIN = '/';
 export const DASHBOARD = "/dashboard/asset";
+export const RECDASHBOARD = "/dashboard/rec";
+export const VULDASHBOARD = "/dashboard/vul";
+export const THREATSDASHBOARD = "/dashboard/threat";
+export const ADDASSET = "/dashboard/addasset";
 export const TEST = '/test';
