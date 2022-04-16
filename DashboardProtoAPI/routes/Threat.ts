244e19c919ff9f421d98a50a263e0208d1c0b250
Hadjira <bkh.hadjira@gmail.com>
2022-04-16 09:19:19 -0400

    add the token for delete , add , update (security)

--- DashboardProtoAPI/routes/Threat.ts
+++ DashboardProtoAPI/routes/Threat.ts
@@ -1,6 +1,6 @@
 import {Router, Request, Response} from 'express'
 import { ThreatApi } from '../controllers/Threat'
-
+const passport = require("passport");
 const threatApi = new ThreatApi();
 
 const threatRouter = Router();
@@ -10,19 +10,19 @@ threatRouter.get("/", (req, res) => {
   threatApi.getAllThreats(req, res);    
 })
 
-threatRouter.get("/:id", (req, res) => {     
+threatRouter.get("/:id" ,passport.authenticate("jwt", { session: false }), (req, res) => {     
   threatApi.getThreatById(req, res);    
 })
 
-threatRouter.post("/", (req, res) =>{
+threatRouter.post("/" ,passport.authenticate("jwt", { session: false }), (req, res) =>{
     threatApi.create(req, res)
 })
 
 
-threatRouter.put("/:id", (req, res) =>{
+threatRouter.put("/:id" ,passport.authenticate("jwt", { session: false }), (req, res) =>{
     threatApi.update(req, res)
 })
-threatRouter.delete('/:id',
+threatRouter.delete('/:id' ,passport.authenticate("jwt", { session: false }),
   (req, res) => threatApi.delete(req, res)
 )
 export default threatRouter;
