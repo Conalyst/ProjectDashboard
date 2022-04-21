00bb1949cc1d9ab6bb8333e0e9b22d26533c5311
Rupi-Jain <rupi.jain@gmail.com>
2022-04-21 15:29:56 -0400

    seed table risk

--- DashboardProtoUI/src/services/constants.js
+++ DashboardProtoUI/src/services/constants.js
@@ -4,19 +4,19 @@ export const GET_ALL_TEST = () => `${BASE_ADDRESS()}api/v2/tests`
 export const LOGIN = ()=>BASE_ADDRESS()+'/v2/login';
 // export const COMPANYASSETS = ()=>BASE_ADDRESS()+'/v2/assets/company/';
 export const COMPANYASSETS = (id)=>`${BASE_ADDRESS()}api/v2/assets/company/${id}`;
-export const GET_ASSET_BY_COMPANY_ID = (companyId)=>BASE_ADDRESS()+'api/v2/dashboard/asset?companyId=${companyId}';
+export const GET_ASSET_BY_COMPANY_ID = (companyId)=>BASE_ADDRESS()+`api/v2/dashboard/asset?companyId=${companyId}`;
 
 export const GET_ALL_ASSETS = ()=>BASE_ADDRESS()+'api/v2/assets';
-export const GET_ASSET_BY_ID = (assetId)=>BASE_ADDRESS()+'api/v2/dashboard/asset?assetId=${assetId}';
+export const GET_ASSET_BY_ID = (assetId)=>BASE_ADDRESS()+`api/v2/dashboard/asset?assetId=${assetId}`;
 export const POST_ASSET= () => `${BASE_ADDRESS()}api/v2/assets`
 
 
 export const GET_ALL_VULNERABILITIES = ()=>BASE_ADDRESS()+'api/v2/vulnerabilities';
-export const GET_VULNERABILITY_BY_ID = (vulnerabilityId)=>BASE_ADDRESS()+'api/v2/vulnerability?vunerabilityId=${vulnerabilityId}';
+export const GET_VULNERABILITY_BY_ID = (vulnerabilityId)=>BASE_ADDRESS()+`api/v2/vulnerability?vunerabilityId=${vulnerabilityId}`;
 export const POST_VULNERABILITY = ()=>BASE_ADDRESS()+'api/v2/vulnerabilities';
 
 export const GET_ALL_THREATS = ()=>BASE_ADDRESS()+'api/v2/threats';
-export const GET_THREAT_BY_ID = (threatId)=>BASE_ADDRESS()+'api/v2/threat?threatId=${threatId}';
+export const GET_THREAT_BY_ID = (threatId)=>BASE_ADDRESS()+`api/v2/threat?threatId=${threatId}`;
 export const POST_THREAT = ()=>BASE_ADDRESS()+'api/v2/threats';
 
 
