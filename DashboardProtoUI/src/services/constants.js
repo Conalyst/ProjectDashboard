fb439e82d90a3da93dd82d21c94a60fd81653825
Rupi-Jain <rupi.jain@gmail.com>
2022-04-25 09:54:30 -0400

    commited

--- DashboardProtoUI/src/services/constants.js
+++ DashboardProtoUI/src/services/constants.js
@@ -1,7 +1,7 @@
 // export const BASE_ADDRESS = () => 'http://localhost:5000/api'
 export const BASE_ADDRESS = () => ''
 export const GET_ALL_TEST = () => `${BASE_ADDRESS()}api/v2/tests`
-export const LOGIN = ()=>BASE_ADDRESS()+'/v2/login';
+export const LOGIN = ()=>BASE_ADDRESS()+'api/v2/login';
 // export const COMPANYASSETS = ()=>BASE_ADDRESS()+'/v2/assets/company/';
 export const COMPANYASSETS = (id)=>`${BASE_ADDRESS()}api/v2/assets/company/${id}`;
 export const GET_ASSET_BY_COMPANY_ID = (companyId)=>BASE_ADDRESS()+`api/v2/dashboard/asset?companyId=${companyId}`;
