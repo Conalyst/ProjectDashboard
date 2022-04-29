b139a5ddef747eb17547dda65b9d66e3202fb884
YBYuan007 <85644458+YBYuan007@users.noreply.github.com>
2022-04-28 23:12:48 -0400

    add vul filter

--- DashboardProtoUI/src/components/VulDashboardDetails.js
+++ DashboardProtoUI/src/components/VulDashboardDetails.js
@@ -6,6 +6,7 @@ import filter_blue from '../images/icons/filter_blue.png';
 import info_white from '../images/icons/outline_info_white.png';
 import pen_white from '../images/icons/pen_white.png';
 import pen_black from '../images/icons/pen_black.png';
+import cancel_icon from "../images/icons/cancel_icon.png";
 import {ADDVUL, EDITVUL} from "../navigation/CONSTANTS";
 import {useHistory} from 'react-router-dom';
 import Info from "./Info";
@@ -16,6 +17,16 @@ import { vulnerabilities as vulnerabilitiesAtom} from '../recoil/atom'
   
 export const VulDashboardDetails = () => {
     const [vulnerabilities, setVulnerabilities] = useRecoilState(vulnerabilitiesAtom);
+
+    const [tempVuls, setTempVuls] = useState(vulnerabilities);
+  const [filterbox, setFilterbox] = useState(false);
+  const [sel, setSel] = useState({
+    selVulCat: "All",
+    selImpact: "All",
+    selLikeli: "All",
+    selRate: "All"
+  });
+
     const storedUser = localStorage.getItem("storedUser");
     const parsedUser = JSON.parse(storedUser);
     const isAdmin = parsedUser.role;
@@ -42,14 +53,280 @@ export const VulDashboardDetails = () => {
       .then((result) => {
           console.log("Vulns", result)
           setVulnerabilities(result);
+          setTempVuls(result);
       })
     }, []);
 
+    function startFilter() {
+      setFilterbox(!filterbox);
+      setTempVuls(vulnerabilities );
+    }
+  
+    function closeFilter() {
+      setFilterbox(!filterbox);
+    }
+  
+    function afterFilter() {
+      setTempVuls(
+        tempVuls
+          .filter(
+            sel.selVulCat != "All"
+              ? (tempVuls) => tempVuls.category == sel.selVulCat
+              : (tempVuls) => tempVuls.category != null
+          )
+          .filter(
+            sel.selImpact != "All"
+              ? (tempVuls) => tempVuls.impact == sel.selImpact
+              : (tempVuls) => tempVuls.impact != null
+          )
+          .filter(
+            sel.selLikeli != "All"
+              ? (tempVuls) => tempVuls.likelihood == sel.selLikeli
+              : (tempVuls) => tempVuls.likelihood != null
+          )
+          .filter(
+            sel.selRate != "All"
+              ? (tempVuls) => tempVuls.rating == sel.selRate
+              : (tempVuls) => tempVuls.rating != null
+          )
+      );
+      console.log("clicked filter", sel);
+    }
+
+    
+
+
     return (
-    <>     
+      <>
+
+      {filterbox ? (
+              <div>
+                <div className="Filter-pop-up-background">
+                  <div className="Filter-fullbox">
+                    <div className="Filter-top">
+                      <div className="Filters-top-wordbox">
+                        <span className="Filter-word">Filters</span>
+                        <img
+                          className="Filter-cancel"
+                          onClick={closeFilter}
+                          src={cancel_icon}
+                          alt=""
+                        />
+                      </div>
+                    </div>
+      
+                    <div className="Filter-middle">
+                      <div className="filter_catergory_frame">
+                        <span className="filter_label"> Vulnerability Category</span>
+                      </div>
+      
+                      <Dropdown>
+                        <Dropdown.Toggle
+                          className="filter_selected"
+                          id="dropdown-basic"
+                          variant="outline-light"
+                        >
+                          {sel.selVulCat}
+                        </Dropdown.Toggle>
+      
+                        <Dropdown.Menu className="filter_dropdown">
+                          <li
+                            onClick={() =>
+                              setSel((prev) => ({ ...prev, selVulCat: "All" }))
+                            }
+                          >
+                            All
+                          </li>
+                          <li
+                            onClick={() =>
+                              setSel((prev) => ({ ...prev, selVulCat: "Operational" }))
+                            }
+                          >
+                            Operational
+                          </li>
+                          <li
+                            onClick={() =>
+                              setSel((prev) => ({
+                                ...prev,
+                                selVulCat: "Personnel",
+                              }))
+                            }
+                          >
+                            Personnel
+                          </li>
+                          <li
+                            onClick={() =>
+                              setSel((prev) => ({
+                                ...prev,
+                                selVulCat: "Technical",
+                              }))
+                            }
+                          >
+                            Technical
+                          </li>
+                   
+                        </Dropdown.Menu>
+                      </Dropdown>
+      
+      
+      
+                      <div className="filter_catergory_frame">
+                        <span className="filter_label"> Impact</span>
+                      </div>
+      
+                      <Dropdown className="filter_selection">
+                        <Dropdown.Toggle
+                          className="filter_selected"
+                          id="dropdown-basic"
+                          variant="outline-light"
+                        >
+                          {sel.selImpact}
+                        </Dropdown.Toggle>
+      
+                        <Dropdown.Menu className="filter_dropdown">
+                          <li
+                            onClick={() =>
+                              setSel((prev) => ({ ...prev, selImpact: "All" }))
+                            }
+                          >
+                            All
+                          </li>
+                          <li
+                            onClick={() =>
+                              setSel((prev) => ({ ...prev, selImpact: "H" }))
+                            }
+                          >
+                            High
+                          </li>
+                          <li
+                            onClick={() =>
+                              setSel((prev) => ({ ...prev, selImpact: "M" }))
+                            }
+                          >
+                            Medium
+                          </li>
+                          <li
+                            onClick={() =>
+                              setSel((prev) => ({ ...prev, selImpact: "L" }))
+                            }
+                          >
+                            Low
+                          </li>
+                        </Dropdown.Menu>
+                      </Dropdown>
+      
+                      <div className="filter_catergory_frame">
+                        <span className="filter_label"> Likelihood </span>
+                      </div>
+      
+                      <Dropdown className="filter_selection">
+                        <Dropdown.Toggle
+                          className="filter_selected"
+                          id="dropdown-basic"
+                          variant="outline-light"
+                        >
+                          {sel.selLikeli}
+                        </Dropdown.Toggle>
+      
+                        <Dropdown.Menu className="filter_dropdown">
+                          <li
+                            onClick={() =>
+                              setSel((prev) => ({ ...prev, selLikeli: "All" }))
+                            }
+                          >
+                            All
+                          </li>
+                          <li
+                            onClick={() =>
+                              setSel((prev) => ({ ...prev, selLikeli: "H" }))
+                            }
+                          >
+                            High
+                          </li>
+                          <li
+                            onClick={() =>
+                              setSel((prev) => ({ ...prev, selLikeli: "M" }))
+                            }
+                          >
+                            Medium
+                          </li>
+                          <li
+                            onClick={() =>
+                              setSel((prev) => ({ ...prev, selLikeli: "L" }))
+                            }
+                          >
+                            Low
+                          </li>
+                        </Dropdown.Menu>
+                      </Dropdown>
+      
+      
+                      <div className="filter_catergory_frame">
+                        <span className="filter_label"> Rating </span>
+                      </div>
+      
+                      <Dropdown className="filter_selection">
+                        <Dropdown.Toggle
+                          className="filter_selected"
+                          id="dropdown-basic"
+                          variant="outline-light"
+                        >
+                          {sel.selRate}
+                        </Dropdown.Toggle>
+      
+                        <Dropdown.Menu className="filter_dropdown">
+                          <li
+                            onClick={() =>
+                              setSel((prev) => ({ ...prev, selRate: "All" }))
+                            }
+                          >
+                            All
+                          </li>
+                          <li
+                            onClick={() =>
+                              setSel((prev) => ({ ...prev, selRate: "H" }))
+                            }
+                          >
+                            High
+                          </li>
+                          <li
+                            onClick={() =>
+                              setSel((prev) => ({ ...prev, selRate: "M" }))
+                            }
+                          >
+                            Medium
+                          </li>
+                          <li
+                            onClick={() =>
+                              setSel((prev) => ({ ...prev, selRate: "L" }))
+                            }
+                          >
+                            Low
+                          </li>
+                        </Dropdown.Menu>
+                      </Dropdown>
+                    </div>
+      
+                    <button
+                      className="Button-Filter"
+                      onClick={() => {
+                        afterFilter();
+                        closeFilter();
+                      }}
+                    >
+                      Filter
+                    </button>
+                  </div>
+                </div>
+              </div>
+            ) : null}
+
+
+
+
         <div className="asset-menu-buttons">
         {(isAdmin === "Admin") && (<button className="Button-Icon-Manage" onClick={onAddVul}>Add Vul</button>  )}
-          <button className="Button-Icon-Filter"> <img  src={filter_blue} alt =""/> Filter</button>
+          <button className="Button-Icon-Filter" onClick={startFilter}> <img  src={filter_blue} alt =""/> Filter</button>
         </div> 
         <div className="table-border-blue scrollable">
         <Table striped hover size="sm" class="table-items-tables-table--column-items">
@@ -73,7 +350,7 @@ export const VulDashboardDetails = () => {
                 </tr>
             </thead>
             <tbody>
-                {vulnerabilities.map((vulnerability) => (
+                {tempVuls.map((vulnerability) => (
                 <tr className="cr-text">
                     <td>
                       <button type="button" className="button-modal" data-bs-toggle="modal" data-bs-target="#exampleModal1"> <img src={info_black} alt =""/></button> 
