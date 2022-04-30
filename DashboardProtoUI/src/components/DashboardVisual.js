90a834a8574566c55a3dd862d246db335a868c2c
Tina <tinasylviac@gmail.com>
2022-04-29 22:06:23 -0400

    Visuals

--- DashboardProtoUI/src/components/DashboardVisual.js
+++ DashboardProtoUI/src/components/DashboardVisual.js
@@ -1,4 +1,5 @@
 import React from "react";
+<<<<<<< HEAD
 import Chart from "react-google-charts";
 
 export const data = [
@@ -22,11 +23,38 @@ export const options = {
         groupWidth: 64
     }
 };
+=======
+import * as crossfilter from "crossfilter2";
+import {csv,timeFormat,timeParse,timeMonth,format} from 'd3'
+import Chart from "react-google-charts";
+import HSBar from "react-horizontal-stacked-bar-chart";
+>>>>>>> 4308267 (Visuals)
 
 export const DashboardVisual = () => {
+       const ldata = [
+        ["Group", "H", "M", "L"],
+        ["Data", 15, 43, 25],
+        ["Network", 30, 24, 12],
+        ["Personnel", 21, 32, 15],
+        ["Software", 10, 43, 30],
+        ["Intangible", 41, 30, 19]
+      ];
+      
+        const options = {
+        chartArea: { width: "75%" },
+        colors: ["#09375f", "#126dba", "#72b7f2"],
+        hAxis: {
+        },
+        vAxis: {
+            minValue: 0
+        },
+        bar: {
+            groupWidth: 64
+        }
+    }
     return (
     <>
-        <div class="asset-rating">
+        <div className="asset-rating">
             <p>Asset Ratings</p>
             <p className="orange-total">Total<br/>82</p>
             <p>High<br/>20</p>
@@ -34,6 +62,7 @@ export const DashboardVisual = () => {
             <p>Low<br/>42</p>
         </div>
         <table className="visual-rating">
+<<<<<<< HEAD
             <tr>
                 <td className="stack-bars-summary">
                     <div className="stack-bar-h">
@@ -80,10 +109,62 @@ export const DashboardVisual = () => {
                     </div>
                 </td>
                 <td className="bar-charts-summary">
+=======
+                 <tr>
+
+
+                 <td className="stack-bars-summary">
+                    <div className="stack-bar-h">
+                    Confidentiality
+                        <div className="V-T-Color">
+                        <HSBar
+          showTextDown       
+          id="hsbarExample"
+          data={[
+            { value: 1, description: "H", color: "#09375f" },
+            { value: 1, description: "M", color: "#126dba" },
+            { value: 2, description: "L", color:"#72b7f2" }
+          ]}
+        />
+                        </div>
+                    </div>
+                    <div className="stack-bar-h">
+                    Integrity
+                        <div className="V-T-Color">
+                        <HSBar
+          showTextDown       
+          id="hsbarExample"
+          data={[
+            { value: 5, description: "H", color: "#09375f" },
+            { value: 5, description: "M", color: "#126dba" },
+            { value: 2, description: "L", color:"#72b7f2" }
+          ]}
+        />
+                        </div>
+                    </div>
+                    <div className="stack-bar-h">
+                    Availability
+                        <div className="V-T-Color">
+                        <HSBar
+          showTextDown       
+          id="hsbarExample"
+          data={[
+            { value: 3, description: "H", color: "#09375f" },
+            { value: 5, description: "M", color: "#126dba" },
+            { value: 2, description: "L", color:"#72b7f2" }
+          ]}
+        />
+                        </div>
+                    </div>
+                </td>
+                
+                    <td className="bar-charts-summary">
+>>>>>>> 4308267 (Visuals)
                     <span>All Categories</span><br/>
                     <Chart
                         chartType="ColumnChart"
                         width="100%"
+<<<<<<< HEAD
                         height="300px"
                         data={data}
                         options={options}
@@ -91,6 +172,18 @@ export const DashboardVisual = () => {
                 </td>
             </tr>
         </table>            
+=======
+                        height="500px"
+                        data={ldata}
+                        options={options}
+                    />
+
+                </td>
+                </tr>
+            </table>
+        
+            
+>>>>>>> 4308267 (Visuals)
     </>
     );
 };
--- DashboardProtoUI/src/components/VulDashboardVisual.js
+++ DashboardProtoUI/src/components/VulDashboardVisual.js
@@ -1,4 +1,5 @@
 import React from "react";
+<<<<<<< HEAD
 import Chart from "react-google-charts";
 
 export const data = [
@@ -20,8 +21,35 @@ export const options = {
         groupWidth: 64
     }
 };
+=======
+import * as crossfilter from "crossfilter2";
+import {csv,timeFormat,timeParse,timeMonth,format} from 'd3'
+import Chart from "react-google-charts";
+import HSBar from "react-horizontal-stacked-bar-chart";
+
+>>>>>>> 4308267 (Visuals)
 
 export const VulDashboardVisual = () => {
+
+  const ldata = [
+    ["Group", "H", "M", "L"],
+    ["Technical", 3, 5, 9],
+    ["Operational", 3, 10, 8],
+    ["Personnel", 2, 7, 5]
+  ];
+  
+  const options = {
+    chartArea: { width: "75%" },
+    colors: ["#09375f", "#126dba", "#72b7f2"],
+    hAxis: {},
+    vAxis: {
+        minValue: 0,
+        ticks: [5, 10]
+    },
+    bar: {
+        groupWidth: 64
+    }
+};
     return (
     <>
         <div class="asset-rating">
@@ -37,6 +65,7 @@ export const VulDashboardVisual = () => {
                     <div className="stack-bar-h">
                         Impact
                         <div className="V-T-Color">
+<<<<<<< HEAD
                             <div className="Dark-Blue-Color">
                             <div className="Light-Blue-Color">
                             <div className="Grey-Color">
@@ -47,10 +76,25 @@ export const VulDashboardVisual = () => {
                                 <span className="value-span-s span-L-s">L</span>
                             </div>
                         </div>
+=======
+                        <HSBar
+          showTextDown
+     
+          id="hsbarExample"
+          data={[
+            { value: 1, description: "H", color: "#09375f" },
+            { value: 5, description: "M", color: "#126dba" },
+            { value: 4, description: "L", color:"#72b7f2" }
+          ]}
+
+        />
+                 </div>
+>>>>>>> 4308267 (Visuals)
                     </div>
                     <div className="stack-bar-h">
                         Likelihood
                         <div className="V-T-Color">
+<<<<<<< HEAD
                             <div className="Dark-Blue-Color">
                             <div className="Light-Blue-Color">
                             <div className="Grey-Color">
@@ -60,6 +104,19 @@ export const VulDashboardVisual = () => {
                                 <span className="value-span-s span-M-s">M</span>
                                 <span className="value-span-s span-L-s">L</span>
                             </div>
+=======
+                        <HSBar
+           showTextDown
+          
+         
+          id="hsbarExample"
+          data={[
+            { value: 80, description: "H", color: "#09375f" },
+            { value: 40, description: "M", color: "#126dba" },
+            { value: 12, description: "L", color:"#72b7f2" }
+          ]}
+        />
+>>>>>>> 4308267 (Visuals)
                         </div>
                     </div>
                 </td>
@@ -69,12 +126,20 @@ export const VulDashboardVisual = () => {
                         chartType="ColumnChart"
                         width="100%"
                         height="300px"
+<<<<<<< HEAD
                         data={data}
+=======
+                        data={ldata}
+>>>>>>> 4308267 (Visuals)
                         options={options}
                     />
                 </td>
             </tr>
+<<<<<<< HEAD
         </table>          
+=======
+        </table>
+>>>>>>> 4308267 (Visuals)
     </>
     );
 };
